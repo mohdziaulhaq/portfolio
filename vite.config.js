06002b758f0ba62653626ee5cd1c11a9c3904ee8
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-08-21 11:57:19 -0500

    portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio-website/',
   plugins: [react()],
-});
+  base: "/portfolio/"
+})
